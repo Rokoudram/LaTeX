634befc0255472abeb20e995d186bf30bf8e58b0
Rokoudram <merlin.olivry.34@gmail.com>
2026-07-31 23:06:47 +0200

    Modification de l'ordre des index (1 avant 10 avant 100)

--- generate_index.py
+++ generate_index.py
@@ -1,16 +1,37 @@
 import os
 import json
+import re
+
+def numero_chapitre(nom):
+    match = re.search(r"chap(\d+)", nom)
+    if match:
+        return int(match.group(1))
+    return 9999
+
 
 for racine, dossiers, fichiers in os.walk("."):
+
+    dossiers[:] = [
+        d for d in dossiers
+        if d not in {".git", ".github"}
+    ]
+
     pdfs = [
         f for f in fichiers
         if f.endswith(".pdf")
     ]
 
     if pdfs:
+        pdfs.sort(key=numero_chapitre)
+
         with open(
             os.path.join(racine, "index.json"),
             "w",
             encoding="utf8"
         ) as fichier:
-            json.dump(pdfs, fichier, indent=2, ensure_ascii=False)
+            json.dump(
+                pdfs,
+                fichier,
+                indent=2,
+                ensure_ascii=False
+            )
